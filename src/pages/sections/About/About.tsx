e122d72a01bf2cf689ac7a399c373e753f35228d
andy-little <69594249+andy-little@users.noreply.github.com>
2022-04-25 14:58:12 +0100

    added bg to about and top-bottom padding to sections with helper class in App.scss

--- src/pages/sections/About/About.tsx
+++ src/pages/sections/About/About.tsx
@@ -1,15 +1,40 @@
 import style from "./About.module.scss";
-import { Section, Socials } from "../../../components/common";
+import { Quote, Section, Socials } from "../../../components/common";
 import Colour from "../../../constants/Colour";
 import { socialIcons } from "../../../constants/SocialIcons";
 
 const About = () => {
     return (
         <Section colour={Colour.PRIMARY}>
-            <h1>About</h1>
-            <div className={style.socials}>
-                <div className={style.icons}>
-                    <Socials data={socialIcons} />
+            <div className={style.container}>
+                <div className="section-padding">
+                    <h1>About</h1>
+                    <section className={style.bio}>
+                        <p>
+                            Mr Little has always had a noisy brain. As a child
+                            he would press his head up to people's ears and ask
+                            if they could hear it. They never could and the
+                            older he grew the more frustrated he became until at
+                            last it was unbearable. Desperate to be heard, he
+                            sold his soul for a band of the finest musicians.
+                        </p>
+                        <p>
+                            Since then Mr Little and his noisy band have been
+                            making music to captivate and spiritualise the
+                            senses, each night giving meaning to their music
+                            which it has never had before and may never have
+                            again.
+                        </p>
+                    </section>
+                    <Quote colour={Colour.PRIMARY} author="Pizza Express Live">
+                        This blistering band blends modern funk and jazz in bold
+                        and daring arrangements.
+                    </Quote>
+                </div>
+                <div className={style.socials}>
+                    <div className={style.icons}>
+                        <Socials data={socialIcons} />
+                    </div>
                 </div>
             </div>
         </Section>
--- src/pages/sections/Album/Album.tsx
+++ src/pages/sections/Album/Album.tsx
@@ -6,24 +6,26 @@ import style from "./Album.module.scss";
 const Album = () => {
     return (
         <Section>
-            <h1>Cuckoo's Nest</h1>
-            <h2>Available Now!</h2>
-            <img
-                className={style.album}
-                src={cuckoo}
-                alt="cuckoo's nest album cover"
-            />
-            <Quote author="the crazy coqs" colour={Colour.PRIMARY}>
-                Fat, greasy, stinking funk, wrapped up in Andy Little's
-                mellifluous arrangements, served up by London's finest.
-            </Quote>
+            <div className="section-padding">
+                <h1>Cuckoo's Nest</h1>
+                <h2>Available Now!</h2>
+                <img
+                    className={style.album}
+                    src={cuckoo}
+                    alt="cuckoo's nest album cover"
+                />
+                <Quote author="the crazy coqs" colour={Colour.PRIMARY}>
+                    Fat, greasy, stinking funk, wrapped up in Andy Little's
+                    mellifluous arrangements, served up by London's finest.
+                </Quote>
 
-            <LinkBtn
-                url="https://www.google.com"
-                options={{ colour: Colour.PRIMARY }}
-            >
-                Listen
-            </LinkBtn>
+                <LinkBtn
+                    url="https://www.google.com"
+                    options={{ colour: Colour.PRIMARY }}
+                >
+                    Listen
+                </LinkBtn>
+            </div>
         </Section>
     );
 };
