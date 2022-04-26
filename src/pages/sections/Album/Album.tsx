cd2b3c2b658d004790cb7be59f5055391b702395
andy-little <69594249+andy-little@users.noreply.github.com>
2022-04-26 13:25:41 +0100

    added grid to album section

--- src/pages/sections/Album/Album.tsx
+++ src/pages/sections/Album/Album.tsx
@@ -6,25 +6,28 @@ import style from "./Album.module.scss";
 const Album = () => {
     return (
         <Section>
-            <div className="section-padding">
-                <h1>Cuckoo's Nest</h1>
-                <h2>Available Now!</h2>
+            <div className={`section-padding ${style.page}`}>
+                <h1 className={style.right}>Cuckoo's Nest</h1>
+                <h2 className={style.right}>Available Now!</h2>
                 <img
-                    className={style.album}
+                    className={`${style.album} ${style.left}`}
                     src={cuckoo}
                     alt="cuckoo's nest album cover"
                 />
-                <Quote author="the crazy coqs" colour={Colour.PRIMARY}>
-                    Fat, greasy, stinking funk, wrapped up in Andy Little's
-                    mellifluous arrangements, served up by London's finest.
-                </Quote>
-
-                <LinkBtn
-                    url="https://www.google.com"
-                    options={{ colour: Colour.PRIMARY }}
-                >
-                    Listen
-                </LinkBtn>
+                <div className={style.right}>
+                    <Quote author="the crazy coqs" colour={Colour.PRIMARY}>
+                        Fat, greasy, stinking funk, wrapped up in Andy Little's
+                        mellifluous arrangements, served up by London's finest.
+                    </Quote>
+                </div>
+                <div className={style.right}>
+                    <LinkBtn
+                        url="https://www.google.com"
+                        options={{ colour: Colour.PRIMARY }}
+                    >
+                        Listen
+                    </LinkBtn>
+                </div>
             </div>
         </Section>
     );
