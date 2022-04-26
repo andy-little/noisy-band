12dd8d4bcbdd4fcbb17ffa7eea03029fb1ea9810
andy-little <69594249+andy-little@users.noreply.github.com>
2022-04-26 12:39:06 +0100

    added bg texture to ep

--- src/pages/sections/EP/EP.tsx
+++ src/pages/sections/EP/EP.tsx
@@ -7,17 +7,22 @@ import ep from "../../../static/images/ep.jpg";
 const EP = () => {
     return (
         <Section>
-            <div className="section-padding">
-                <h1 className={style.h1}>Darker Than Blue</h1>
-                <h2>New EP Coming Soon!</h2>
-                <img
-                    className={style.album}
-                    src={ep}
-                    alt="darker than blue EP cover"
-                />
-                <Quote author="shades of soul LTD" colour={Colour.OFF_WHITE}>
-                    Don't miss this amazing array of talent.
-                </Quote>
+            <div className={style.bg}>
+                <div className="section-padding">
+                    <h1 className={style.h1}>Darker Than Blue</h1>
+                    <h2>New EP Coming Soon!</h2>
+                    <img
+                        className={style.album}
+                        src={ep}
+                        alt="darker than blue EP cover"
+                    />
+                    <Quote
+                        author="shades of soul LTD"
+                        colour={Colour.OFF_WHITE}
+                    >
+                        Don't miss this amazing array of talent.
+                    </Quote>
+                </div>
             </div>
         </Section>
     );
