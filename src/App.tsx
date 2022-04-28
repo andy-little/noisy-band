f49aeeeb683633e6f721b3ac913bd98d7c3a6f29
andy-little <69594249+andy-little@users.noreply.github.com>
2022-04-28 09:55:24 +0100

    added basic navBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./style/App.scss";
 import { Routes, Route } from "react-router-dom";
 import { Footer, Home } from "./pages/sections";
+import { NavBar } from "./components";
 
 const App = () => {
     return (
         <>
+            <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import style from "./NavBar.module.scss";
+import { navItems } from "../../constants/nav";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-    return <div>NavBar</div>;
+    return (
+        <nav className={style.nav_container}>
+            <div className={style.nav_items}>
+                {navItems.map(({ url, name, id }) => {
+                    return (
+                        <Link className={style.link} to={url} key={id}>
+                            {name}
+                        </Link>
+                    );
+                })}
+            </div>
+        </nav>
+    );
 };
 
 export default NavBar;
