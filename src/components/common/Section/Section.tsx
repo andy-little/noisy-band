dd358c6fdb7e2e51bb6d4fcc47ea6dd63b51c41e
andy-little <69594249+andy-little@users.noreply.github.com>
2022-04-29 09:42:51 +0100

    aaded ability to add class to section component

--- src/components/common/Section/Section.tsx
+++ src/components/common/Section/Section.tsx
@@ -5,10 +5,15 @@ import style from "./Section.module.scss";
 interface SectionProps {
     children: React.ReactNode;
     colour?: Colour;
+    className?: string;
 }
-const Section: React.FC<SectionProps> = ({ children, colour }) => {
+const Section: React.FC<SectionProps> = ({ children, colour, className }) => {
     return (
-        <article className={`${style.section} ${colour && style[colour]}`}>
+        <article
+            className={`${style.section} ${
+                colour && style[colour]
+            } ${className}`}
+        >
             {children}
         </article>
     );
