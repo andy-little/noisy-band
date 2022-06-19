daa3018a7df34d10523df3ce8ca1207b5341e914
andy-little <69594249+andy-little@users.noreply.github.com>
2022-06-19 17:29:38 +0100

    created function to take care of setting location and scrolling but has bug

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,36 +1,36 @@
-import React from "react";
-import style from "./NavBar.module.scss";
-import { navItems } from "../../constants/nav";
-import { Link } from "react-router-dom";
-import { RiMenu5Fill } from "react-icons/ri";
-import Location from "../../constants/Location";
+import React from 'react';
+import style from './NavBar.module.scss';
+import { navItems } from '../../constants/nav';
+import { Link } from 'react-router-dom';
+import { RiMenu5Fill } from 'react-icons/ri';
+import Location from '../../constants/Location';
 
 interface NavBarProps {
-    cb: (value: Location) => void;
+  cb: (value: Location) => void;
 }
 
 const NavBar: React.FC<NavBarProps> = ({ cb }) => {
-    return (
-        <nav className={style.nav_container}>
-            <div className={`${style.clip}`}>
-                <RiMenu5Fill className={style.hamburger} />
-                <div className={style.nav_items}>
-                    {navItems.map(({ url, name, id, location }) => {
-                        return (
-                            <Link
-                                className={style.link}
-                                to={url}
-                                key={id}
-                                onClick={() => cb(location)}
-                            >
-                                {name}
-                            </Link>
-                        );
-                    })}
-                </div>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className={style.nav_container}>
+      <div className={`${style.clip}`}>
+        <RiMenu5Fill className={style.hamburger} />
+        <div className={style.nav_items}>
+          {navItems.map(({ url, name, id, location }) => {
+            return (
+              <Link
+                className={style.link}
+                to={url}
+                key={id}
+                onClick={() => cb(location)}
+              >
+                {name}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
--- src/contexts/navContext.tsx
+++ src/contexts/navContext.tsx
@@ -1,28 +1,42 @@
-import React, { createContext, useContext, useState } from "react";
-import Location from "../constants/Location";
+import React, { createContext, useContext, useState } from 'react';
+import Location from '../constants/Location';
 
 interface ContextInterface {
-    location: Location;
-    setLocation: (value: Location) => void;
+  location: Location;
+  setLocation: (value: Location) => void;
+  setScrollToY: (value: number) => void;
+  goToSection: (value: Location) => void;
 }
 const NavCtx = createContext<ContextInterface>({
-    location: 0,
-    setLocation: () => {},
+  location: 0,
+  setLocation: () => {},
+  goToSection: () => {},
+  setScrollToY: () => {},
 });
 
 interface NavProvProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const NavProvider: React.FC<NavProvProps> = ({ children }) => {
-    const [location, setLocation] = useState(Location.TOP);
-    return (
-        <NavCtx.Provider value={{ location, setLocation }}>
-            {children}
-        </NavCtx.Provider>
-    );
+  const [location, setLocation] = useState(Location.TOP);
+  const [scrollToY, setScrollToY] = useState(0);
+  const goToSection = (loc: Location) => {
+    console.log(scrollToY);
+
+    setLocation(loc);
+    console.log(scrollToY);
+    window.scrollTo(0, scrollToY);
+  };
+  return (
+    <NavCtx.Provider
+      value={{ location, setLocation, goToSection, setScrollToY }}
+    >
+      {children}
+    </NavCtx.Provider>
+  );
 };
 
 export const useNavCtx = () => {
-    return useContext(NavCtx);
+  return useContext(NavCtx);
 };
