9928528d7c77b74a3c0edb0b99ec2a9feeb2418c
andy-little <69594249+andy-little@users.noreply.github.com>
2022-07-02 18:35:10 +0100

    added unstyled mob menu

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './NavBar.module.scss';
 import { navItems } from '../../constants/nav';
 import { Link } from 'react-router-dom';
@@ -6,31 +6,35 @@ import { HashLink } from 'react-router-hash-link';
 import { RiMenu5Fill } from 'react-icons/ri';
 import Location from '../../constants/Location';
 
-// interface NavBarProps {
-//   cb: (value: Location) => void;
-// }
+const NavBarItems: React.FC = () => {
+  return (
+    <>
+      {navItems.map(({ url, name, id, location }) => {
+        const hasID = location !== Location.TOP;
+        const Component = hasID ? HashLink : Link;
+        return (
+          <Component className={style.link} to={url} key={id}>
+            {name}
+          </Component>
+        );
+      })}
+    </>
+  );
+};
 
-const NavBar: React.FC /* <NavBarProps> */ = (/* { cb } */) => {
+const NavBar: React.FC = () => {
+  const [isMobNavOpen, setIsMobNavOpen] = useState(false);
+
   return (
     <nav className={style.nav_container}>
       <div className={`${style.clip}`}>
-        <RiMenu5Fill className={style.hamburger} />
+        <RiMenu5Fill
+          className={style.hamburger}
+          onClick={() => setIsMobNavOpen(!isMobNavOpen)}
+        />
+        <div className={style.mob_nav}>{isMobNavOpen && <NavBarItems />}</div>
         <div className={style.nav_items}>
-          {navItems.map(({ url, name, id, location }) => {
-            const hasID = location !== Location.TOP;
-            const Component = hasID ? HashLink : Link;
-
-            return (
-              <Component
-                className={style.link}
-                to={url}
-                key={id}
-                // onClick={() => cb(location)
-              >
-                {name}
-              </Component>
-            );
-          })}
+          <NavBarItems />
         </div>
       </div>
     </nav>
