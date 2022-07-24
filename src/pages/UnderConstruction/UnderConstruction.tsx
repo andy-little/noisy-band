662e036a86f54e3065f353c9786bd7b97d863506
andy-little <69594249+andy-little@users.noreply.github.com>
2022-07-24 16:17:02 +0100

    styled under construction page

--- src/pages/UnderConstruction/UnderConstruction.tsx
+++ src/pages/UnderConstruction/UnderConstruction.tsx
@@ -1,21 +1,24 @@
-import React from "react";
-import style from "./UnderConstruction.module.scss";
-import { LinkBtn } from "../../components/common";
-import Colour from "../../constants/Colour";
+import React from 'react';
+import style from './UnderConstruction.module.scss';
+import { LinkBtn } from '../../components/common';
+import Colour from '../../constants/Colour';
 
 const UnderConstruction = () => {
-    return (
-        <div className="page">
-            <h1>Page Unavailable</h1>
-            <p>
-                This page is currently under construction. Please check back
-                again soon!
-            </p>
-            <LinkBtn options={{ colour: Colour.PRIMARY }} url="/">
-                Home
-            </LinkBtn>
-        </div>
-    );
+  return (
+    <div className={`page ${style.container}`}>
+      <h1 className={style.h1}>Page Unavailable</h1>
+      <p className={style.p}>
+        This page is currently under construction. Please check back again soon!
+      </p>
+      <LinkBtn
+        className={style.link}
+        options={{ colour: Colour.PRIMARY }}
+        url="/"
+      >
+        Home
+      </LinkBtn>
+    </div>
+  );
 };
 
 export default UnderConstruction;
