36a5eae453b7fefeeb9d4216658768b931e4488a
andy-little <69594249+andy-little@users.noreply.github.com>
2022-07-30 08:26:41 +0100

    reduced image sizes

--- src/pages/sections/Footer/Footer.tsx
+++ src/pages/sections/Footer/Footer.tsx
@@ -1,22 +1,22 @@
-import { Socials } from "../../../components/common";
-import { socialAndContactIcons } from "../../../constants/SocialIcons";
-import style from "./Footer.module.scss";
-import logo from "../../../static/logo.png";
+import { Socials } from '../../../components/common';
+import { socialAndContactIcons } from '../../../constants/SocialIcons';
+import style from './Footer.module.scss';
+import logo from '../../../static/logo-sm.png';
 
 const Footer = () => {
-    return (
-        <div className={`${style.footer}`}>
-            <div className={style.copyright}>
-                <p>Copyright © 2022 Andy Little</p>
-                <p>All Rights Reserved</p>
-                {/*  <p> Site By Andy Little</p> */}
-            </div>
-            <img className={style.logo} src={logo} alt="noisy band logo" />
-            <div className={style.socials}>
-                <Socials data={socialAndContactIcons} />
-            </div>
-        </div>
-    );
+  return (
+    <div className={`${style.footer}`}>
+      <div className={style.copyright}>
+        <p>Copyright © 2022 Andy Little</p>
+        <p>All Rights Reserved</p>
+        {/*  <p> Site By Andy Little</p> */}
+      </div>
+      <img className={style.logo} src={logo} alt="noisy band logo" />
+      <div className={style.socials}>
+        <Socials data={socialAndContactIcons} />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/sections/Hero/Hero.tsx
+++ src/pages/sections/Hero/Hero.tsx
@@ -1,15 +1,15 @@
-import logo from "../../../static/images/logo.png";
-import style from "./Hero.module.scss";
-import { Section } from "../../../components/common";
-import Colour from "../../../constants/Colour";
+import logo from '../../../static/logo.png';
+import style from './Hero.module.scss';
+import { Section } from '../../../components/common';
+import Colour from '../../../constants/Colour';
 
 const Hero = () => {
-    return (
-        <Section colour={Colour.PRIMARY} className={style.container}>
-            <img className={style.logo} src={logo} alt="noisy band logo" />
-            <h1 className={style.title}>Funk - Soul - Jazz</h1>
-        </Section>
-    );
+  return (
+    <Section colour={Colour.PRIMARY} className={style.container}>
+      <img className={style.logo} src={logo} alt="noisy band logo" />
+      <h1 className={style.title}>Funk - Soul - Jazz</h1>
+    </Section>
+  );
 };
 
 export default Hero;
